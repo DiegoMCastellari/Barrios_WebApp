271c8f4c8d55baccb7d4a4b4cd33fabdff5025e7
DiegoMCastellari <agrimcastellari@gmail.com>
2023-10-05 15:02:06 -0300

    actualizacion dropdown

--- dropdown.js
+++ dropdown.js
@@ -23,41 +23,56 @@ function selectOption() {
     // get a selected option and text value using the text property
     let selectedValue = div_dropdown.options[selectedIndex].text;
 
-    sidevar_data_update(selectedValue)
-    div_info.style.display = "block";
-    /* div_map.style.width = map_reduce; */
-    div_map.style.display = "block";   
-
-    CABA_br.resetStyle()
-    CABA_br_zones.resetStyle()
-    styleDropdown(CABA_br, selectedValue)
-    styleDropdown(CABA_br_zones, selectedValue)
-    
     let north = -34.5474261;
     let east = -58.35041293;
     let west = -58.52739985;
     let sud = -34.69163551;
-    CABA_br.eachLayer(function (layer) {  
-        layer.setStyle({color : 'grey'})
-        if(layer.feature.properties.NOM_MAP == selectedValue) {    
-            layer.setStyle({color : 'yellow'})    
-            
-            let boundary = layer.getBounds()
-            let bound_north = boundary._northEast.lat
-            let bound_east = boundary._northEast.lng 
-            let bound_sud = boundary._southWest.lat
-            let bound_west = boundary._southWest.lng
-
-            if (bound_north < north ){north = bound_north}
-            if (bound_east < east ){east = bound_east}
-            if (bound_sud > sud ){sud = bound_sud}
-            if (bound_west > west ){west = bound_west}
-        }         
-    })
-    east = east-((east-west)/2)
     let corner1 = L.latLng(sud, west);
     let corner2 = L.latLng(north, east);
     let bounds = L.latLngBounds(corner1, corner2);
+
+    if (selectedValue != "Ninguna - Seleccionar Barrio"){
+        sidevar_data_update(selectedValue)
+        div_info.style.display = "block";
+        /* div_map.style.width = map_reduce; */
+        div_map.style.display = "block";   
+
+        CABA_br.resetStyle()
+        CABA_br_zones.resetStyle()
+        styleDropdown(CABA_br, selectedValue)
+        styleDropdown(CABA_br_zones, selectedValue)
+        
+        
+        CABA_br.eachLayer(function (layer) {  
+            layer.setStyle({color : 'grey'})
+            if(layer.feature.properties.NOM_MAP == selectedValue) {    
+                layer.setStyle({color : 'yellow'})    
+                
+                let boundary = layer.getBounds()
+                let bound_north = boundary._northEast.lat
+                let bound_east = boundary._northEast.lng 
+                let bound_sud = boundary._southWest.lat
+                let bound_west = boundary._southWest.lng
+
+                if (bound_north < north ){north = bound_north}
+                if (bound_east < east ){east = bound_east}
+                if (bound_sud > sud ){sud = bound_sud}
+                if (bound_west > west ){west = bound_west}
+            }         
+        })
+        east = east-((east-west)/2)
+        corner1 = L.latLng(sud, west);
+        corner2 = L.latLng(north, east);
+        bounds = L.latLngBounds(corner1, corner2);
+    } else {
+        if (symbologyType == 'TipoBarrio'){
+            CABA_br.resetStyle()
+            CABA_br_zones.resetStyle()
+        } else if (symbologyType == 'Comunas'){
+            symbolByComuna(CABA_br)
+            symbolByComuna(CABA_br_zones)
+        }
+    }
     mymap.flyToBounds(bounds);
  }
 
